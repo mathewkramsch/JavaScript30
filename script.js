fefe40019b91ebb44e4e8440540a4321c207c966
Mathew Kramsch <mathewkramsch@ucsb.edu>
2020-12-15 00:30:56 -0800

    finished project

---
+++ script.js
@@ -0,0 +1,44 @@
+// script.js
+
+
+const playSound = (keyCodeNum)=>{
+    const audio = document.querySelector(`audio[data-key='${keyCodeNum}']`);
+    const keyButton = document.querySelector(`.key[data-key='${keyCodeNum}']`);
+    if (!audio) return;
+
+    audio.currentTime=0;  // re-loads audio to play from beginning
+    audio.play();
+    keyButton.classList.toggle('playing');
+    showWord();
+}
+
+function removePlayIndication(e) {  // use function declaration so this refers to object called
+    if (e.propertyName=='transform') {
+        this.classList.remove('playing');
+        document.querySelector('p').textContent = '';
+    }
+}
+
+const randomAdLib = ()=>{
+    const adLibs = ['brrrrr', 'yuh', 'yeet', 'okay', 'skirt', 'its lit', 'gratataa', 'igh', 'bow'];
+    const choice = Math.round(Math.random()*(adLibs.length-1));
+    return adLibs[choice];
+}
+
+const showWord = ()=>{
+    const p = document.querySelector('p');
+    p.textContent = randomAdLib();
+}
+
+
+window.addEventListener('keydown', (keyEvent)=>{
+    playSound(keyEvent.keyCode);
+});
+
+const keys = document.querySelectorAll('.key');
+keys.forEach( keyButton => {
+    keyButton.addEventListener('click', ()=>{
+        playSound(keyButton.getAttribute('data-key'));
+    });
+    keyButton.addEventListener('transitionend', removePlayIndication);
+});
